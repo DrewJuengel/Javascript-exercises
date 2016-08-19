6cedb44bf59b99e41609a29951e46b66393cbd1c
Drew Juengel <Drew Juengel>
2016-08-19 15:45:57 -0600

    midday2

--- week2/simple-validator/Validator.js
+++ week2/simple-validator/Validator.js
@@ -1,2 +1,6 @@
-var PhoneNumber = prompt("Enter your phone number. This won't be used for spam, calm down.");
+function PhoneNumber(){
+    console.log("working")
+    var cell = prompt("Whats your number?")
+    if(cell.charAt(3) === "-" && cell.charAt(7) === "-" && cell.length === 12){}
+}
 
