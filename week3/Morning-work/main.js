c6d0294788f7dc2109378320523931829381df74
Drew Juengel <Drew Juengel>
2016-08-23 11:09:42 -0600

    Morning commit

---
+++ week3/Morning-work/main.js
@@ -0,0 +1,10 @@
+angular.module("Missouri", [])
+    .controller("Colorado", ColoradoController)
+
+function ColoradoController() {
+    console.info("Colorado is cool");
+
+    var Col = this;
+
+    
+}
